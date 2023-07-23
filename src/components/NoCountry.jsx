ea3ff0feb2634746d79de0b02ca35fbeee4980ae
CosmicEntity <cosmicentity19@gmail.com>
2023-07-23 23:16:28 +0530

    directory issuse fix

--- src/components/NoCountry.jsx
+++ src/components/NoCountry.jsx
@@ -10,7 +10,7 @@ function NoArticle() {
   ) : (
     <div className="w-4/5 m-auto">
       <img
-        src="./images/world.svg"
+        src="../../assets/images/world.svg"
         alt="Search Country"
         className=" w-full md:w-1/2 mx-auto"
       />
